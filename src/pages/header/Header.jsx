6fdffbb2fc2e739becc67422a5eb4b7b04ade6e0
tuan <rickytuan1991@gmail.com>
2022-08-31 18:33:47 +0300

    'done'

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -5,10 +5,10 @@ export default function header() {
   let rightInfor = ["introduction", "solution", "plan", "rectangle", "login", "apply"];
   return (
     <div className={style.box}>
-      <div style={{ gridColumn: 0 / 2 }}>
+      <div style={{ gridColumn: 1 / 2 }} className={style.left}>
         <img className={style.logo} src={require("../../img/logo.png")} alt="logo"></img>
       </div>
-      <div style={{ gridColumn: 3 }}>
+      <div style={{ gridColumn: 2/3}} className={style.right}>
         <ul style={{ display: "flex", listStyleType: "none", gap: "10px" }}>
           {rightInfor.map((item, index) => {
             return (
